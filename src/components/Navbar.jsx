946f2ca51bbb78f27d9820a0301be1e19fcc05b5
Arman Ali <armanali0178614@gmail.com>
2025-12-19 23:04:40 +0530

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React from "react";
 import { FaUser } from "react-icons/fa";
 import { HiSun } from "react-icons/hi";
 import { RiSettings3Fill } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="nav flex items-center justify-between px-[100px] h-[90px] border-b-[1px] border-gray-800">
-        <div className="logo">
-            <h3 className='text-[25px] font-[700] sp-text'>GenUI</h3>
+    <div
+      className="relative nav flex items-center justify-between
+      px-4 sm:px-6 md:px-[100px]
+      h-[70px] md:h-[90px]
+      border-b border-gray-800"
+    >
+      <h3
+        className="
+          absolute left-1/4 -translate-x-1/2
+          md:static md:translate-x-0
+          text-[20px] sm:text-[22px] md:text-[25px]
+          font-[700] sp-text
+        "
+      >
+        GenUI
+      </h3>
+
+      <div className="icons flex items-center gap-3 sm:gap-4 md:gap-[15px] ml-auto">
+        <div className="icon text-[18px] sm:text-[20px] cursor-pointer">
+          <HiSun />
         </div>
-        <div className="icons flex items-center gap-[15px]">
-            <div className="icon"><HiSun /></div>
-            <div className="icon"><FaUser /></div>
-            <div className="icon"><RiSettings3Fill /></div>
+        <div className="icon text-[18px] sm:text-[20px] cursor-pointer">
+          <FaUser />
+        </div>
+        <div className="icon text-[18px] sm:text-[20px] cursor-pointer">
+          <RiSettings3Fill />
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
